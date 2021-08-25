6abe93d01acab171677729bad232a7b5c21bd0a1
jarghiskhan <jared.l.wyatt@outlook.com>
2021-08-24 22:40:10 -0400

    Initial data loading

---
+++ app/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import QuoteList from './components/QuoteList';
+
+
+
+function App() {
+  return (
+    <>
+    <div className="header">
+      <h1>Anime Quote List</h1>
+    </div>
+    <div>
+      <QuoteList></QuoteList>
+    </div>
+    </>
+  );
+}
+
+export default App;
